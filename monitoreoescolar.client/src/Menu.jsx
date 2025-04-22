ad589e7c741b7eea1696721353f7d6747da110ca
Jose Ramon <joseramonmoracruz02@gmail.com>
2025-04-22 11:12:48 -0600

    Cambios de estilo en el menu

--- monitoreoescolar.client/src/Menu.jsx
+++ monitoreoescolar.client/src/Menu.jsx
@@ -1,20 +1,22 @@
 ﻿import { Link } from "react-router-dom";
 import "./Menu.css";
+import Logo from "./assets/logo_proyecto.png";
 
 const Menu = () => {
     return (
-        <>
-            <nav className="menu">
-                <ul>
-                    <li><Link to="/subir-calif">Subir calificaciones</Link></li>
-                    <li><Link to="/generar-reportes">Reporte de Conducta</Link></li>
-                    <li><Link to="/notificaciones">Envío de notificaciones</Link></li>
-                    <li><Link to="/agregar-alumno">Agregar Alumno</Link></li>
-                    <li><Link to="/lista-alumnos">Lista de Grupos</Link></li>
-                    <li><Link to="/">Cerrar Sesión</Link></li> 
-                </ul>
-            </nav>
-        </>
+        <nav className="menu">
+            <div className="logo-section">
+                <img src={Logo} alt="Logo" className="logo" />
+            </div>
+            <ul className="menu-links">
+                <li><Link to="/subir-calif">Subir Calificaciones</Link></li>
+                <li><Link to="/generar-reportes">Reporte de Conducta</Link></li>
+                <li><Link to="/notificaciones">Envío de Notificaciones</Link></li>
+                <li><Link to="/agregar-alumno">Agregar Alumno</Link></li>
+                <li><Link to="/lista-alumnos">Lista de Grupos</Link></li>
+                <li><Link to="/">Cerrar Sesión</Link></li>
+            </ul>
+        </nav>
     );
 };
 
